8ca457a35c0f1461a2ec089c62dfd9771d183a5a
Andressa Aquino <andressam.aquino@gmail.com>
2019-09-12 19:31:27 -0300

    Adding ADC and AND instructions

--- src/cpu.py
+++ src/cpu.py
@@ -21,14 +21,14 @@ class CPU:
     def log(self, address = None):
         if (address):
             p = self.flagController.getFlagsStatusByte()
-            logls(self.a.value, self.x.value, self.y.value, self.sp.value, ceil(self.pc.value / 2), p, address, self.memory.get_memory_at_position(address).value)
+            logls(self.a.value, self.x.value, self.y.value, self.sp.value, ceil(self.pc.value / 2), p, address, self.memory.get_memory_at_position_str(address).value)
         else:
             p = self.flagController.getFlagsStatusByte()
             log(self.a.value, self.x.value, self.y.value, self.sp.value, ceil(self.pc.value / 2), p)
 
     def get_next_byte(self):
         begin = self.pc.value
-        self.pc.value = self.pc.value + 2
+        self.pc.value = self.pc.value + 2 
         end = self.pc.value
         byte = self.program_code[begin:end].upper()
         return byte
@@ -41,9 +41,24 @@ class CPU:
     def handleInstructionAdcImmediate(self):
         byte = self.get_next_byte()
         immediate = int(byte, 16)
+        self.adc(immediate)
+
+    def handleInstructionAdcZeroPage(self):
+        address = self.get_next_byte()
+        value = self.memory.get_memory_at_position_str(address).value
+        self.adc(value)
+
+    def handleInstructionAdcZeroPageX(self):
+        byte = self.get_next_byte()
+        addressStart = int(byte, 16)
+        address = (addressStart + self.x.value) & 0xFF
+        value = self.memory.get_memory_at_position_int(address).value
+        self.adc(value)
+
+    def adc(self, value):
         carry = self.flagController.getCarryFlag()
         aOldValue = self.a.value
-        sum = aOldValue + immediate + carry
+        sum = aOldValue + value + carry
         self.a.value = sum & 0xFF                           # set a value limiting to one byte            
          
         self.flagController.setNegativeIfNeeded(self.a.value) # set negative flag
@@ -56,11 +71,25 @@ class CPU:
             self.flagController.clearCarryFlag()
 
         # set overflow flag
-        if (aOldValue ^ immediate) & 0x80 == 0 and (aOldValue ^ self.a.value) & 0x80 != 0:
+        if (aOldValue ^ value) & 0x80 == 0 and (aOldValue ^ self.a.value) & 0x80 != 0:
             self.flagController.setOverflowFlag()
         else:
             self.flagController.clearOverflowFlag()
             
+    ## AND Instructions
+
+    def handleInstructionAndImmediate(self):
+        byte = self.get_next_byte()
+        self.a.value = int(byte, 16) & self.a.value
+        self.flagController.setNegativeIfNeeded(self.a.value) # set negative flag
+        self.flagController.setZeroFlagIfNeeded(self.a.value) # set zero flag
+
+    def handleInstructionAndZeroPage(self):
+        address = self.get_next_byte()
+        value = self.memory.get_memory_at_position_str(address).value
+        self.a.value = value & self.a.value
+        self.flagController.setNegativeIfNeeded(self.a.value) # set negative flag
+        self.flagController.setZeroFlagIfNeeded(self.a.value) # set zero flag
 
     ## INC Instructions
     def handleInstructionINX(self):
@@ -106,6 +135,11 @@ class CPU:
         immediate = int(byte, 16)
         self.x.value = immediate
 
+    def handleInstructionLDXZeroPage(self):
+        address = self.get_next_byte()
+        value = self.memory.get_memory_at_position_str(address)
+        self.x.value = value.value
+
     def handleInstructionLDYImmediate(self):
         byte = self.get_next_byte()
         immediate = int(byte, 16)
@@ -114,13 +148,13 @@ class CPU:
     ## Store Instructions
     def handleInstructionSTXZeroPage(self):
         byte = self.get_next_byte()
-        self.memory.set_memory_at_position(byte, self.x)
+        self.memory.set_memory_at_position_str(byte, self.x)
 
     def handleInstructionSTXZeroPageY(self):
         byte = self.get_next_byte()
 
         address = format((int(byte, 16) + self.y.value), '04x')
-        self.memory.set_memory_at_position(address, self.x)
+        self.memory.set_memory_at_position_str(address, self.x)
         self.log(address)
 
     def handleInstructionSTXAbsolute(self):
@@ -129,17 +163,17 @@ class CPU:
 
         address = (high_byte + low_byte)
 
-        self.memory.set_memory_at_position(address, self.x)
+        self.memory.set_memory_at_position_str(address, self.x)
 
     def handleInstructionSTYZeroPage(self):
         byte = self.get_next_byte()
-        self.memory.set_memory_at_position(byte, self.y)
+        self.memory.set_memory_at_position_str(byte, self.y)
 
     def handleInstructionSTYZeroPageY(self):
         byte = self.get_next_byte()
 
         address = format((int(byte, 16) + self.x.value), '04x')
-        self.memory.set_memory_at_position(address, self.y)
+        self.memory.set_memory_at_position_str(address, self.y)
 
     def handleInstructionSTYAbsolute(self):
         low_byte = self.get_next_byte()
@@ -147,7 +181,7 @@ class CPU:
 
         address = (high_byte + low_byte)
 
-        self.memory.set_memory_at_position(address, self.y)
+        self.memory.set_memory_at_position_str(address, self.y)
 
     ## Jump Instructions
     def handleInstructionJmpAbsolute(self):
@@ -161,8 +195,8 @@ class CPU:
         byte = self.get_next_byte()
         byte_h = str(int(byte, 16) + 1)
         
-        low_byte = str(self.memory.get_memory_at_position(byte))
-        high_byte = str(self.memory.get_memory_at_position(byte_h))
+        low_byte = str(self.memory.get_memory_at_position_str(byte))
+        high_byte = str(self.memory.get_memory_at_position_str(byte_h))
 
         address = (high_byte + low_byte)
         self.pc.value = int(address, 16) * 2
@@ -179,6 +213,10 @@ class CPU:
             # LDX Immediate
             elif instruction == 'A2':
                 self.handleInstructionLDXImmediate()
+
+            # LDX Zero Page
+            elif instruction == 'A6':
+                self.handleInstructionLDXZeroPage()
 
             # LDY Immediate
             elif instruction == 'A0':
@@ -197,7 +235,7 @@ class CPU:
                 self.handleInstructionSTXAbsolute()
 
             # STY Zero page
-            elif instruction == '86':
+            elif instruction == '84':
                 self.handleInstructionSTYZeroPage()
 
             # STY Zero page X
@@ -255,6 +293,42 @@ class CPU:
             # ADC Immediate
             elif instruction == '69':
                 self.handleInstructionAdcImmediate()
+
+            # ADC Zero Page
+            elif instruction == '65':
+                self.handleInstructionAdcZeroPage()
+
+            # ADC Zero Page X
+            elif instruction == '75':
+                self.handleInstructionAdcZeroPageX()
+
+            # # ADC Absolute
+            # elif instruction == '6D':
+            #     self.handleInstructionAdcAbsolute()
+
+            # # ADC Absolute X
+            # elif instruction == '7D':
+            #     self.handleInstructionAdcAbsoluteX()
+
+            # # ADC Absolute Y
+            # elif instruction == '79':
+            #     self.handleInstructionAdcAbsoluteY()
+
+            # # ADC Indirect X
+            # elif instruction == '61':
+            #     self.handleInstructionAdcIndirectX()
+
+            # # ADC Indirect Y
+            # elif instruction == '71':
+            #     self.handleInstructionAdcIndirectY()
+
+            # AND Immediante
+            elif instruction == '29':
+                self.handleInstructionAndImmediate()
+
+            # AND Zero Page
+            elif instruction == '25':
+                self.handleInstructionAndZeroPage()
             
             self.log()
             instruction = self.get_next_byte()
--- src/memory.py
+++ src/memory.py
@@ -4,8 +4,15 @@ class Memory:
     def __init__(self):
         self.memory = [0]*1024*64    # 64 KB
 
-    def get_memory_at_position(self, pos) -> c_uint8:
+    def get_memory_at_position_str(self, pos) -> c_uint8:
         return self.memory[int(pos, 16)]
     
-    def set_memory_at_position(self, pos, data):
+    def set_memory_at_position_str(self, pos, data):
         self.memory[int(pos, 16)] = data
+
+    def get_memory_at_position_int(self, pos) -> c_uint8:
+        return self.memory[pos]
+    
+    def set_memory_at_position_int(self, pos, data):
+        self.memory[pos] = data
+        
