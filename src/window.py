5c25353cd4598a6cbe0218adca7da602938b45a7
Andressa Aquino <andressam.aquino@gmail.com>
2019-10-15 20:01:08 -0300

    Reading input from the keyboard

--- src/window.py
+++ src/window.py
@@ -1,4 +1,5 @@
 import pyglet
+from pyglet.window import key
 
 class Window(pyglet.window.Window):
 
@@ -6,8 +7,25 @@ class Window(pyglet.window.Window):
         super(Window, self).__init__()
         self.label = pyglet.text.Label('Hello, world!', x=10, y=10)
 
+    # Enter is start
+    # Space is select
     def on_key_press(self, symbol, modifiers):
-        print('A key was pressed')
+        if symbol == key.A:
+            print('The A key was pressed.')
+        elif symbol == key.B:
+            print('The B arrow key was pressed.')
+        elif symbol == key.LEFT:
+            print('The left arrow key was pressed.')
+        elif symbol == key.RIGHT:
+            print('The right arrow key was pressed.')
+        elif symbol == key.UP:
+            print('The up arrow key was pressed.')
+        elif symbol == key.DOWN:
+            print('The down arrow key was pressed.')
+        elif symbol == key.ENTER:
+            print('The enter key was pressed.')
+        elif symbol == key.SPACE:
+            print('The space key was pressed.')
 
     def on_draw(self):
         self.clear()
